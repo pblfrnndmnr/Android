ea988c77f44a69fed538b94932f527e199c8249d
root <root@localhost>
2016-06-01 19:53:15 -0300

    Initial commit

---
+++ app/src/main/java/com/pfm/iremoteudp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pfm.iremoteudp;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
