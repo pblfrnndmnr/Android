a2ce2a037a3ded9721ea7740895f9f09a44858b1
pblfrnndmnr <pablomanieri@gmail.com>
2016-06-24 22:34:52 -0300

    Cambios menores

--- src/main/java/com/pfm/iremoteudp/IrRemoteControlUDP.java
+++ src/main/java/com/pfm/iremoteudp/IrRemoteControlUDP.java
@@ -5,7 +5,7 @@
  *  International Inc.  By accepting transfer of this copy, Recipient agrees
  *  to retain this software in confidence, to prevent disclosure to others,
  *  and to make no use of this software other than that for which it was
- *  delivered.  This is an unpublished copyrighted work of Digi International
+ *   delivered.  This is an unpublished copyrighted work of Digi International
  *  Inc.  Except as permitted by federal law, 17 USC 117, copying is strictly
  *  prohibited.
  *
